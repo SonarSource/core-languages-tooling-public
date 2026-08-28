900ded044d32e0d0d90f9d15c34e3d65c368d2b5
rombirli <romain.birling@sonarsource.com>
2026-08-28 10:42:06 +0200

    Fix QG

--- pvf-comment/pvf_comment_parser.py
+++ pvf-comment/pvf_comment_parser.py
@@ -60,7 +60,7 @@ def _parse_payload(payload: list[str], rule_prefixes: list[str]) -> PvfCommentPa
                     # raise if token doesn't match "<prefix>%d"
                     int(token[len(rp):])
                     return True
-                except:
+                finally:
                     pass
         return False
 
